6c36c756f12079df464de957b1b811afa4b4f6c1
Miriam Briskman <mary060196@gmail.com>
2021-04-13 04:26:13 -0400

    fixing README/prog.cpp typos

--- README.md
+++ README.md
@@ -1,39 +1,43 @@
-# Evidence of Algorithm's Correctness: PDA for Words NOT in {a^nb^n, n>=0}
+# Algorithm: PDA for Words NOT in {a^nb^n, n>=0}
 
 <b>Created by:</b> Miriam Briskman
 
-<b>Date:</b>: 04/13/2021
+<b>Date:</b> 04/13/2021
 
-## Info
+## In a Few Words
 
 This repo contains a simple `C++` program testing a PDA for recognizing words not of the form `a^nb^n, n>= 0`.
 The program accepts a natural number (`m >= 1`), and recursively creates strings over `{a,b}` of sizes less than or equal to `m`. In other words, it creates all the strings in the power set of `{a,b}`. Then, the program tests the algorithm on each of those strings to check if the word represented by the string is accepted or rejected.
 
+The most important value of this algorithm is that it is <b>deterministic</b> (it does not use backtracing.)
+
 ## The Algorithm
 Here is a pseudo-code ("informal description") of the PDA:
-- Begin the PDA by pushing a `$` sign into the stack. 
+1. Begin the PDA by pushing a `$` sign into the stack. 
 
-- For each char in the input string:
-  - If this is `a`:
-    - If `b` was the most recently read char before this `a`:
-      - Accept the word (since we got a substring of the form `ba`, which is not allowed in `a^nb^n`.)
-    - Otherwise, push an `a` onto the stack. 
-  - Otherwise, for each encountered b, pop an `a` from the stack. 
-    - If a `$` symbol is popped from the stack:
-      - Accept the word (the string begins with b's or there are more b's than a's.) 
+2. For each char in the input string:
+   - If this is `a`:
+     - If `b` was the most recently read char before this `a`:
+       - Accept the word (since we got a substring of the form `ba`, which is not allowed in `a^nb^n`.)
+     - Otherwise, push an `a` onto the stack. 
+   - Otherwise, for each encountered b, pop an `a` from the stack. 
+     - If a `$` symbol is popped from the stack:
+       - Accept the word (the string begins with `b`s or there are more `b`s than `a`s.) 
 
-- When there are no more symbols to read from the input string, pop a symbol from the stack.
-- If this symbol is an `a`:
-  - Accept the word (there are more a's than b's.)
-  - If this is the `$` symbol, reject the word (there are as many a's as b's.)
+3. When there are no more symbols to read from the input string, pop a symbol from the stack.
+4. If this symbol is an `a`:
+   - Accept the word (there are more `a`s than `b`s.)
+5. If this is the `$` symbol:
+   - Reject the word (there are as many `a`s as `b`s.)
 
 ## Get Started
 
-1. Do git clone
-2. In a terminal, compile as `g++ -o prog prog.c`.
-3. Run as `prog.exe NUM` (Windows) or `./prog NUM` (Mac, Linux)
+1. Using your terminal, type: `git clone https://github.com/mary060196/not-apownbpown-algo`.
+2. `cd` into `not-apownbpown-algo/`
+3. Compile the program as `g++ -o prog prog.c`.
+4. Run as `prog.exe NUM` (Windows) or `./prog NUM` (Mac, Linux)
    Note that NUM is an integer, such as 1, 2, 3, etc.
-4. You can add the option `-c` or `--comments`, such as in: `./prog -c NUM` or `./prog -- comments NUM`
+5. You can add the option `-c` or `--comments`, such as in: `./prog -c NUM` or `./prog --comments NUM`
    to see all the stack operations (what was pushed or popped before the program accepted/rejected the word.)
 
 ## Example
@@ -75,4 +79,5 @@ The word: bbbb is accepted!
 <hr>
 
 Thanks for reading! :)
+
 -- Miriam Briskman
--- prog.cpp
+++ prog.cpp
@@ -1,14 +1,16 @@
 // Miriam Briskman, 04/13/2021
+// Simulator of PDA accepting words NOT in {a^nb^n | n >= 0}
 // The input alphabet is {a, b}.
-// The stack alphabet is {$, a, #}.
+// The stack alphabet is {$, a}.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stack> // stack
+#include <stdio.h> // For 'printf'
+#include <stdlib.h> // For 'malloc', 'free', EXIT_SUCCESS, etc.
+#include <string.h> // For 'strcmp' to check for cmd options
+#include <stack> // stack data structure
 
 using namespace std;
 
+// Global variable (set only once, in 'main')
 bool are_comments_on = false;
 
 // Function Prototypes:
@@ -35,6 +37,7 @@ int main (int argc, char * argv[])
     printf ("The empty word is rejected! (it's of the form a^0b^0)\n", ptr);
     // Call the recursive function:
     recur_func(max_size, 0, ptr);
+    free(ptr);
     return EXIT_SUCCESS;
 }
 
@@ -64,7 +67,7 @@ void algo (int size, char * ptr)
     /* The algorithm: */
     the_stack.push('$'); // Begin the PDA by pushing a $ sign into the stack.
     if (are_comments_on)
-       printf("[Pushed $");
+        printf("[Pushed $");
     for (int i = 0; i < size; i++)
     {
        if (ptr[i] == 'a') // For each encountered 'a' in the input string,
@@ -77,17 +80,20 @@ void algo (int size, char * ptr)
               printf ("The word: %s is accepted!\n", ptr); // The word is immediatelly accepted!
               return;
           }
-          if (are_comments_on)                                                                                                       printf(", pushed a");
+          if (are_comments_on)
+              printf(", pushed a");
           recent_read = 'a';
        }
        else // Otherwise, for each encountered b,
        {
           recent_popped = the_stack.top(); // pop an a from the stack.
           the_stack.pop();
-          if (are_comments_on)                                                                                                       printf(", popped %c", recent_popped);
+          if (are_comments_on)
+              printf(", popped %c", recent_popped);
           if (recent_popped == '$') // If a $ symbol is popped from the stack,
           {
-             if (are_comments_on)                                                                                                       printf("]\n");
+             if (are_comments_on)
+                 printf("]\n");
              // accept the word (the string begins with b's or there are more b's than a's.)
              printf ("The word: %s is accepted!\n", ptr);
              return;
@@ -98,9 +104,10 @@ void algo (int size, char * ptr)
     // When there are no more symbols to read from the input string,
     recent_popped = the_stack.top(); // pop a symbol from the stack.
     the_stack.pop();
-    if (are_comments_on)                                                                                                       printf(", {Last popping} popped %c]\n", recent_popped);
+    if (are_comments_on)
+        printf(", {Last popping} popped %c]\n", recent_popped);
     if (recent_popped == 'a') // If this symbol is an a,
-      printf ("The word: %s is accepted!\n", ptr); // accept the word (there are more a's than b's.)
+        printf ("The word: %s is accepted!\n", ptr); // accept the word (there are more a's than b's.)
     else // If this is the $ symbol,
-      printf ("The word: %s is rejected!\n", ptr); // reject the word (there are as many a's as b's.)
+        printf ("The word: %s is rejected!\n", ptr); // reject the word (there are as many a's as b's.)
 }
